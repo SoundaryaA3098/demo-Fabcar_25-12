b97e89148497b382405a7fe34a0cb16b07329a93
soundarya <soundarya.ayyappan@intainft.com>
2020-01-08 14:37:03 +0530

    adding file

---
+++ demo-Fabcar_25-12/demo-Fabcar/evenorodd.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class evenorodd{
+    public static void main(String[] args){
+        int num=10;
+        if(num%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
